523df2eb0aa2d06af6bd2c81e7160f4305f9e463
Andy Wu <awu200592@gmail.com>
2025-01-23 17:16:22 -0500

    hello world

---
+++ labs/rec01-C++_basics/rec01.cpp
@@ -0,0 +1,95 @@
+/*
+  rec01_start.cpp
+  fall 2024
+ Andy Wu
+ cw4483
+*/
+
+#include <iostream>
+
+//
+// main
+//
+int main() { // Yes, it has to have an int for the return type
+    std::cout << "hello world" << std:endl;
+    //
+    // Output
+    //
+
+    // Task 1. Printing hello ...  No use of "using namespace"
+
+
+    // Task 2  Printing hello ...  Using "using namespace"
+
+
+
+    //
+    // Types and variables
+    //
+
+    // Task 3  Displaying uninitialized variable
+
+    
+    // Task 4 Use the sizeof function to see the size in bytes of
+    //        different types on your machine.
+
+    
+    // Task 5  Attempt to assign the wrong type of thing to a variable
+
+
+
+    //
+    // Conditions
+    //
+
+    // Task 6  Testing for a range
+
+    
+
+    //
+    // Loops and files
+    //
+
+    // Task 7  Looping, printing values 10 to 20 inclusive
+
+    // First with a for loop
+
+    // Then with a while loop
+
+    // Finally with a do-while loop
+    
+
+
+    // Task 8  Looping to successfully open a file, asking user for the name
+
+
+    // Task 9  Looping, reading file word by "word".
+
+
+    // Task 10 Open a file of integers, read it in, and display the sum.
+
+
+    // Taks 11 Open and read a file of integers and words. Display the sum.
+
+
+    //
+    // Vectors
+    //
+
+    // Task 12 Filling a vector of ints
+
+    
+    // Task 13 Displaying the vector THREE times
+    //         a) using the indices,
+
+    //         b) using a "ranged for"
+
+    //         c) using indices again but backwards
+
+
+    // Task 14. Initialize a vector with the primes less than 20.
+
+    
+} // main
+
+
