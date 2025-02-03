bd7333051c817c72608b62b5c4b0518696bc850d
waterprooftoaster <awu200592@gmail.com>
2025-02-03 00:52:08 -0500

    completed all 3 tasks all checks passed

--- labs/rec02-structs,_vectors_and_functions/rec02.cpp
+++ labs/rec02-structs,_vectors_and_functions/rec02.cpp
@@ -13,7 +13,7 @@
 using namespace std;
 
 struct Hydrocarbon{
-  int H, C;
+  int C, H;
   vector<string> names;
 
   Hydrocarbon(int num_of_C, int num_of_H, vector<string> hydrocarbon_names) : 
@@ -32,17 +32,20 @@ struct Hydrocarbon{
   }
 };
 
-
 //function headers
 void open_file(ifstream &ifs);
-Hydrocarbon create_hydrocarbon(const string &name, int C, int H);
-vector<Hydrocarbon> read_hydrocarbs(ifstream &ifs);
-void print_hydrocarbs(const vector<Hydrocarbon> &hydrocarbons);
+Hydrocarbon create_hydrocarbon(const string& name, int C, int H);
+vector<Hydrocarbon> read_hydrocarbs(ifstream& ifs);
+void combine_hydrocarbons(vector<Hydrocarbon>& hydrocarbs);
+void print_hydrocarbs(const vector<Hydrocarbon>& hydrocarbons);
+void sort_hydrocarbs(vector<Hydrocarbon>& hydrocarbs);
 
 //main
 int main(){
   ifstream hydrocarb_stream;
   vector<Hydrocarbon> hydrocarbs = read_hydrocarbs(hydrocarb_stream);
+  combine_hydrocarbons(hydrocarbs);
+  sort_hydrocarbs(hydrocarbs);
   print_hydrocarbs(hydrocarbs);
 }
 
@@ -66,30 +69,59 @@ Hydrocarbon create_hydrocarbon(const string &name, int C, int H){
 }
 
 vector<Hydrocarbon> read_hydrocarbs(ifstream &ifs){
-  open_file (ifs);
+  open_file(ifs);
 
   int H, C;
   string name;
   char ignore;
   vector<Hydrocarbon> hydrocarbons;
 
-  while (ifs >> name >> ignore >> C >> ignore >> H){
-
-    for (Hydrocarbon curr : hydrocarbons){
-      if (C == curr.C && H == curr.H){
-        curr.add_name(name);
-
-      } else {
-        Hydrocarbon new_hydrocarbon = create_hydrocarbon(name, C, H);
-        hydrocarbons.push_back(new_hydrocarbon);
-      }
-    }
+  while(ifs >> name >> ignore >> C >> ignore >> H){
+    Hydrocarbon new_hydrocarbon = create_hydrocarbon(name, C, H);
+    hydrocarbons.push_back(new_hydrocarbon);
   }
+
   return hydrocarbons;
 }
 
-void sort_hydrocarbs(){
-  //sort
+void combine_hydrocarbons(vector<Hydrocarbon>& hydrocarbs){
+  //x = curr index, i = index for searching
+  for(size_t x = 0; x < hydrocarbs.size(); x++){
+    for(size_t i = 0; i < hydrocarbs.size(); i++){
+
+      //add name if same formula
+      if (hydrocarbs[i].C == hydrocarbs[x].C && hydrocarbs[i].H == hydrocarbs[x].H && i != x){
+        hydrocarbs[x].add_name((hydrocarbs[i].names)[0]);
+
+        //remove duplicate index
+        size_t remove = i;
+        hydrocarbs.erase(hydrocarbs.begin() + remove);
+      }
+    }
+  }
+}
+
+void sort_hydrocarbs(vector<Hydrocarbon>& hydrocarbs){
+  //bubble sort
+  size_t n = hydrocarbs.size();
+
+  for (size_t i = 0; i < n - 1; i++){
+
+    for (size_t j = 0; j < n - i - 1; j++){
+
+      if (hydrocarbs[j].C > hydrocarbs[j+1].C){
+        swap(hydrocarbs[j], hydrocarbs[j+1]);
+      }
+
+      //if num of C is equal, sort based on H
+      if (hydrocarbs[j].C == hydrocarbs[j+1].C){
+
+        if(hydrocarbs[j].H > hydrocarbs[j+1].H){
+          swap(hydrocarbs[j], hydrocarbs[j+1]);
+        }
+      }
+    }
+  }
 }
 
 void print_hydrocarbs(const vector<Hydrocarbon> &hydrocarbons){
