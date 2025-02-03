a796c81e7fe038e2676f47d8425d1dbe596bc11f
waterprooftoaster <awu200592@gmail.com>
2025-02-02 22:22:38 -0500

    task 2 first try

--- labs/rec02-structs,_vectors_and_functions/rec02.cpp
+++ labs/rec02-structs,_vectors_and_functions/rec02.cpp
@@ -1,8 +1,8 @@
 /*
   rec02.cpp
-  fall 2024
- Andy Wu
- cw4483
+  spring 2025
+  Andy Wu
+  cw4483
 */
 
 #include <iostream>
@@ -12,7 +12,77 @@
 #include <vector>
 using namespace std;
 
+struct Hydrocarbon{
+  int H, C;
+  vector<string> names;
+
+  Hydrocarbon(int num_of_C, int num_of_H, vector<string> hydrocarbon_names) : 
+  C(num_of_C), H(num_of_H), names(hydrocarbon_names){}
+
+  void display() const{
+    cout << "C" << C << "H" << H;
+    for (const string& name : names){
+      cout << " " << name;
+    }
+    cout << endl;
+  }
+
+  void add_name(const string &name) {
+    names.push_back(name);
+  }
+};
+
+void open_file(ifstream &ifs){
+  do{
+    ifs.open("hydrocarbs.txt");
+
+    if(!ifs){
+      cerr << "could not open file" << endl;
+    }
+
+  }while(!ifs);
+}
+
+//function headers
+Hydrocarbon create_hydrocarbon(const string &name, int C, int H);
+vector<Hydrocarbon> read_hydrocarbs(ifstream &ifs);
+
+//main
 int main(){
-    cout << " is this shit finally working" << endl;
-    return 0;
+  
+}
+
+Hydrocarbon create_hydrocarbon(const string &name, int C, int H){
+  //push name into new vector to match struct type
+  vector<string> names;
+  names.push_back(name);
+
+  //create new hydocarb
+  Hydrocarbon new_hydrocarbon(C, H, names);
+  return new_hydrocarbon;
+}
+
+vector<Hydrocarbon> read_hydrocarbs(ifstream &ifs){
+  open_file (ifs);
+
+  int H, C;
+  string name;
+  char ignore;
+  vector<Hydrocarbon> hydrocarbons;
+
+  while (ifs >> name >> ignore >> C >> ignore >> H){
+    for (Hydrocarbon curr : hydrocarbons){
+      if (C == curr.C && H == curr.H){
+        curr.add_name(name);
+
+      } else {
+        Hydrocarbon new_hydrocarbon = create_hydrocarbon(name, C, H);
+        hydrocarbons.push_back(new_hydrocarbon);
+      }
+    }
+  }
+}
+
+void sort_hydrocarbs(){
+  //sort
 }
