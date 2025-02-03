2b4c4d202da33440ab9c1b049b7fd6e5f824ee16
waterprooftoaster <awu200592@gmail.com>
2025-02-02 20:38:16 -0500

    new compiler

--- labs/rec02-structs,_vectors_and_functions/rec02.cpp
+++ labs/rec02-structs,_vectors_and_functions/rec02.cpp
@@ -12,50 +12,7 @@
 #include <vector>
 using namespace std;
 
-struct Hydrocarbon{
-    Hydrocarbon(int num_of_carbon, int num_of_hydrogen)
-    : C(num_of_carbon), H(num_of_hydrogen){}
-    
-    void add_name(const string& new_name){
-        names.push_back(new_name);
-    }
-    
-    void print_molecules() const{
-        cout << "C" << C << "H" << H;
-        for (const string& name : names){
-            cout << " " << name;
-        }
-        cout << endl;
-    }
-    
-    int get_hydrogen() const {
-        return H;
-    }
-    
-    int get_carbon() const {
-        return C;
-    }
-    
-private:
-    int C;
-    int H;
-    vector<string> names;
-}
-    
 int main(){
-
-    vector<string> molecules;
-    
-    ifstream ifs;
-    ifs.open("text.txt");
-
-    if (!ifs){
-        cout << "unable to open file" << endl;
-    }
-
-    string token;
-    while (ifs >> token){
-#add
-    }
-
+    cout << " is this shit finally working" << endl;
+    return 0;
 }
