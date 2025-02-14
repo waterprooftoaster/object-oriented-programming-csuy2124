d2ed103b73b5fa7bc83c1423a181fe4f80fbbede
Andy Wu <awu200592@gmail.com>
2025-02-14 14:27:26 -0500

    initialized lab 4

---
+++ labs/rec04-pointer_tutorial/rec04_start.cpp
@@ -0,0 +1,195 @@
+// Task 1
+// Who are you? What is this program called?
+
+// I have stopped using cerr in your started code because CLion makes
+// a mess with it.
+
+#include <iostream>   // needed for task 2
+#include <vector>     // needed for task 27
+using namespace std;  // needed for task 2
+
+// Task 16 Complex definition
+
+
+// Task 17 PlainOldClass definition
+
+
+// Task 18 PlainOldClass2 definition
+
+
+// Task 28 Colour definition
+
+
+// Task 29 SpeakerSystem definition
+
+// Task 29 Amplifier definition
+
+
+// Task 30 Person Definition
+
+
+int main() {
+    cout << "Task 1\n";
+
+    // Task 2
+    cout << "Task 2\n";
+    // Add code here
+
+    
+    // Task 3 
+    cout << "Task 3\n";
+    // Using the debugger, set a breakpoint in main and run to
+    // it. Observe the value of x.
+    // [No new code]
+
+    // Task 4
+    cout << "Task 4\n";
+    // Using the debugger, step forward observing the value of x
+    // [No new code]
+
+    // Task 5
+    cout << "Task 5\n";
+    // Watch window. Xcode equivalent? Gdb equivalent?
+    // Activity: see x's value
+    // [No new code]
+
+    // Task 6
+    cout << "Task 6\n";
+    // Watch window to see the address of x
+    // [No new code]
+
+    // Task 7
+    cout << "Task 7\n";
+    // Watch window to see the value stored at the address of x
+    // [No new code]
+    
+    // Task 8
+    cout << "Task 8\n";
+    // Add code. here
+
+    
+    // Task 9
+    cout << "Task 9\n";
+    // Using the value you observe in &x, write a line of code to
+    // directly assign that value to p. E.g. p = 0x0012fed4;
+    // What happens? Should have a compiler error.
+
+    
+    // Task 10
+    cout << "Task 10\n";
+    // Run earlier print statement for "p = ". Observe the watch
+    // window.
+    // Then add code here:
+
+    
+    // Task 11
+    cout << "Task 11\n";
+    // Add code here
+
+    
+    // Task 12
+    cout << "Task 12\n";
+    // Add code
+
+    
+    // Task 13
+    cout << "Task 13\n";
+    // Add code here
+
+    
+    // Task 14
+    cout << "Task 14\n";
+    // Add code here
+
+    
+    // Task 15
+    cout << "Task 15!!!\n"; // Yes, this is a VERY IMPORTANT task!
+    // Const and pointers.
+    // Add code here
+
+    
+    // Task 16
+    cout << "Task 16\n";
+    // Pointers to user-defined types.  Add the type for struct Complex above.
+    // Add code here
+
+    
+    // Task 17
+    cout << "Task 17\n";
+    // Add code here
+    // What is this code doing?
+    
+
+    // Task 18
+    cout << "Task 18\n";
+    // Test the changes to PlainOldClass
+    
+    
+    // Task 19
+    cout << "Task 19\n";
+    // creating and modifying an item on the heap. Use the debugger to
+    // observe.
+    // Add code here
+
+
+    // Task 20
+    cout << "Task 20\n";
+    // What happens to pDyn in the following? With most compilers it
+    // is unchanged.  Visual Studio actually changes it. (I should
+    // check if that is just in debug mode.)
+    // Add code here
+
+
+    // Task 21
+    cout << "Task 21\n";
+    // Add code here
+    
+
+    // Task 22
+    cout << "Task 22\n";
+    // Add code here
+    
+    
+    // Task 23
+    cout << "Task 23\n";
+    // Question. Can <code>this</code> be nullptr?
+    
+
+    // Task 24
+    cout << "Task 24\n";
+    // Add code here
+
+
+    // Task 25
+    cout << "Task 25\n";
+    // Add code here
+    
+
+    // Task 26
+    cout << "Task 26\n";
+    // Add code here
+    
+
+    // Task 27
+    cout << "Task 27\n";
+    // Add code here
+
+
+    // Task 28 Use the Colour class
+    cout << "Task 28\n";
+    // Add Colour class above main
+    // Add code here
+    
+
+    // Task 29 Use SpeakerSystem and Amplifier classes
+    cout << "Task 29\n";
+    // Add SpeakerSystem and Amplifier classes above main
+
+
+    // Task 30 Use the Person class
+    cout << "Task 30\n";
+    // Add Person class above main
+    // Add code here
+
+    
+}
