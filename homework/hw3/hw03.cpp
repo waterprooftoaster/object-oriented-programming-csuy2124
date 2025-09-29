45b0c455d4e6a6f8bcaf738a8b1a2cfbfd696bfc
Chengtai Wu <cw4483@nyu.edu>
2025-09-29 15:03:38 -0400

    small changes based on feedback

--- homework/hw3/hw03.cpp
+++ homework/hw3/hw03.cpp
@@ -2,7 +2,6 @@
     Andy Wu
     cw4483
     hw03.cpp
-    Spring 2025
  */
 
 #include <fstream>
@@ -18,14 +17,12 @@ public:
     }
 
     //methods
-    int get_strength() const {
+    const int get_strength() const {
         return weapon.get_strength();
     }
-
-    string get_name() const {
+    const string& get_name() const {
         return warrior_name;
     }
-
     void set_strength(int strength) {
         weapon.set_strength(strength);
     }
@@ -38,42 +35,33 @@ public:
 
         cout << warrior1_name << " battles " << warrior2_name << endl;
 
-        if ((warrior1_strength == 0 && warrior2_strength != 0)
-            || (warrior1_strength != 0 && warrior2_strength == 0)) {
+        // if one warrior is already dead
+        if (warrior1_strength == 0 ^ warrior2_strength == 0){ 
             if (warrior1_strength == 0) {
                 cout << "He's dead, " << warrior2_name << endl;
-            }
-
-            if (warrior2_strength == 0) {
+            } else {
                 cout << "He's dead, " << warrior1_name << endl;
             }
-
-            return; //prevents further checks
-            }
-
+            return;
+        } 
+        // if both warriors are dead
         if (warrior1_strength == 0 && warrior2_strength == 0) {
             cout << "Oh, NO! They're both dead! Yuck!" << endl;
-
-            return; //prevents further checks
+            return;
         }
-
         if (warrior1_strength == warrior2_strength) {
             cout << "Mutual Annihilation: " << warrior1_name << " and "
                     << warrior2_name << " die at each other's hands" << endl;
             set_strength(0);
             warrior2->set_strength(0);
-
-            return; //prevents further checks
+            return; 
         }
-
         if (warrior1_strength > warrior2_strength) {
             cout << warrior1_name << " defeats " << warrior2_name << endl;
             set_strength(warrior1_strength - warrior2_strength);
             warrior2->set_strength(0);
-
-            return; //prevents further checks
+            return;
         }
-
         if (warrior2_strength > warrior1_strength) {
             cout << warrior2_name << " defeats " << warrior1_name << endl;
             warrior2->set_strength(warrior2_strength - warrior1_strength);
@@ -81,7 +69,7 @@ public:
         }
     }
 
-private:
+private: // to nest weapon
     class Weapon {
     public:
         Weapon(const string &the_weapon_name, int the_strength)
@@ -89,53 +77,33 @@ private:
         }
 
         //methods
-        int get_strength() const {
+        const int get_strength() const {
             return strength;
         }
-
-        string get_name() const {
+        const string& get_name() const {
             return weapon_name;
         }
-
-        void set_strength(const int &new_strength) {
+        void set_strength(const int new_strength) {
             strength = new_strength;
         }
 
     private:
         friend ostream &operator <<(ostream &os, const Weapon &weapon);
-
         string weapon_name;
         int strength;
-    };
+    }; // class Weapon
 
+private: // for warrior fields
     friend ostream &operator <<(ostream &os, const Warrior &warrior);
     friend ostream &operator <<(ostream &os, const Weapon &weapon);
-
     string warrior_name;
     Weapon weapon;
 };
 
-void open_file(const ifstream &file);
-
-ostream &operator <<(ostream &os, const Warrior &warrior) {
-    os << "Warrior: " << warrior.warrior_name << ", " << warrior.weapon;
-    return os;
-}
-
-ostream &operator <<(ostream &os, const Warrior::Weapon &weapon) {
-    os << "weapon: " << weapon.weapon_name << ", " << weapon.strength << endl;
-    return os;
-}
-
-void open_file(ifstream &file) {
-    file.clear();
-    file.open("warriors.txt");
-    if (!file) {
-        cerr << "Error opening file" << endl;
-        exit(1);
-    }
-}
-
+// func headers
+void open_file(ifstream &file);
+ostream &operator <<(ostream &os, const Warrior::Weapon &weapon);
+ostream &operator <<(ostream &os, const Warrior &warrior);
 
 int main() {
     vector<Warrior> warriors;
@@ -179,4 +147,23 @@ int main() {
             }
         }
     }
+}
+
+void open_file(ifstream &file) {
+    file.clear();
+    file.open("warriors.txt");
+    if (!file) {
+        cerr << "Error opening file" << endl;
+        exit(1);
+    }
+}
+
+ostream &operator <<(ostream &os, const Warrior &warrior) {
+    os << "Warrior: " << warrior.warrior_name << ". " << warrior.weapon;
+    return os;
+}
+
+ostream &operator <<(ostream &os, const Warrior::Weapon &weapon) {
+    os << "Weapon: " << weapon.weapon_name << ", " << weapon.strength << endl;
+    return os;
 }
